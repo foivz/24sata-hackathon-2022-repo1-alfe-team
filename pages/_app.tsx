5f0ec8cb950d2c7a981f96bbd268e6bbe63617b6
rokobekavac0 <roko.bekavac2003@gmail.com>
2022-04-02 14:43:28 +0200

    added team, add team

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,10 @@ import { ChakraProvider, Container } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "../styles/globals.css";
+
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
@@ -12,6 +15,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 				<ChakraProvider>
 					<Container maxW="container.xl" minH="90vh">
 						<Component {...pageProps} />
+						<ToastContainer />
 					</Container>
 				</ChakraProvider>
 			</QueryClientProvider>
