c97905efa07d849248943015a7fb6b81fa939c0f
Marko Pekas <45564032+MarkoPekas@users.noreply.github.com>
2022-04-02 20:34:03 +0200

    barchart

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider, Container } from "@chakra-ui/react";
+import { ChakraProvider, Container, extendTheme } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
@@ -9,10 +9,22 @@ import "../styles/globals.css";
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+	const theme = extendTheme({
+		colors: {
+			brand: {
+				500: "#E5F64A",
+				600: "#C4E935",
+				700: "#A4D921",
+				800: "#7BCB0E",
+				900: "#5BB705",
+			}
+		}
+	});
+
 	return (
 		<SessionProvider session={session}>
 			<QueryClientProvider client={queryClient}>
-				<ChakraProvider>
+				<ChakraProvider theme={theme} >
 					<Container maxW="container.xl" minH="90vh" paddingX={0}>
 						<Component {...pageProps} />
 						<ToastContainer />
