70a22aef83edad9e789e0a3dbc757bb8e77c2e9b
Flegar <81305836+Flegar@users.noreply.github.com>
2022-04-02 16:19:21 +0200

    Update MobileNav.tsx

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -19,9 +19,9 @@ import {
 } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { useSession, signIn, signOut } from "next-auth/react";
-import { BiHomeSmile } from 'react-icons/bi';
+import { VscHome } from 'react-icons/vsc';
 import { BsPlusSquare } from 'react-icons/bs';
-import { FiSettings } from 'react-icons/fi';
+import { IoSettingsOutline } from 'react-icons/io5';
 import { IconButton } from '@chakra-ui/react';
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 
@@ -36,9 +36,9 @@ export default function MobileNav() {
       <Box bg={useColorModeValue("gray.100", "gray.900")} px={4} pos="fixed" bottom={0} width="full" left={0}>
         <Flex h={16} alignItems={"center"} justifyContent={"center"}>
           <Stack direction={"row"} justifyContent={"space-around"} width="full">
-            <IconButton aria-label='Home' icon={<BiHomeSmile />} />
-            <IconButton aria-label='Add new' icon={<BsPlusSquare />} />
-            <IconButton aria-label='Setting' icon={<FiSettings />} />
+            <IconButton aria-label='Home' icon={<VscHome size="32px" />} size="lg" />
+            <IconButton aria-label='Add new' icon={<BsPlusSquare size="32px" />} size="lg" />
+            <IconButton aria-label='Setting' icon={<IoSettingsOutline size="32px" />} size="lg" />
           </Stack>
         </Flex>
       </Box>
